d5b70c7c2143ce770cad5995a1e5e8d2c1483316
RyanVNgo <vanryan711@gmail.com>
2025-04-15 16:43:21 -0700

    Finished model implementation

--- SISR/models/sisr_model.py
+++ SISR/models/sisr_model.py
@@ -24,27 +24,36 @@ class SISRModel():
             case _:
                 self.network = PlainNet(c_in)
 
-        self.device = torch.device('cpu')
-        if device is None:
-            if torch.cuda.is_available():
-                self.device = torch.device('cuda')
-        else:
+        if device is not None:
             try:
                 self.device = torch.device(device)
             except:
                 pass
+        else:
+            if torch.cuda.is_available():
+                self.device = torch.device('cuda')
+            else:
+                self.device = torch.device('cpu')
 
         self.network.to(self.device)
+
+
+    def set_eval(self):
+        self.network.eval()
+
+
+    def set_train(self):
+        self.network.train(True)
 
 
     def predict(self, input):
         if input.shape[-3] != self.c_in:
             return input
+        return self.network(input)
 
-        self.network.eval()
-        with torch.no_grad():
-            output = self.network(input).cpu()
-        return output
+    
+    def get_parameters():
+        return self.network.parameters()
 
 
     def get_modules(self):
@@ -64,6 +73,14 @@ class SISRModel():
 
     def input_channel_depth(self):
         return self.c_in
+
+
+    def save_model(self, path):
+        torch.save(self.network.state_dict(), path)
+
+
+    def load_model(self, path):
+        model.load_state_dict(torch.load(path, weights_only=True))
 
 
 def sisr_network_types():
--- SISR/train.py
+++ SISR/train.py
@@ -15,27 +15,40 @@ from data import data_loader
 def main():
     print(f"Running {os.path.basename(__file__)}")
 
+    # Placeholder to verify model variations
+    batch_size = 1
+    input_channels = 3
+    patch_size = 256
+    input_dim = [batch_size, input_channels, patch_size, patch_size]
+
     sisr_models = []
     for net_type in models.sisr_network_types():
-        sisr_models.append(models.SISRModel(net_type, c_in = 3))
+        sisr_models.append(models.SISRModel(net_type, c_in = input_channels))
 
     for idx, model in enumerate(sisr_models):
         print(f'Model {idx}:')
         print(f'    Network type: {model.network_type()}')
         print(f'    Channel depth: {model.input_channel_depth()}')
         print(f'    Current Device: {model.curr_device()}')
-        print(f'    Integrity test:')
 
+        runs = 20
+        print(f'    Integrity test:')
+        print(f'        Performing {runs} predicitions...')
+
+        model.set_eval()
         predict_times = []
-        for _ in range(20):
-            input = torch.rand(1, 3, 256, 256).to(model.curr_device())
+        preds = []
+        for _ in range(runs):
+            input = torch.rand(input_dim).to(model.curr_device())
             start_time = time.time()
-            preds = model.predict(input).detach().numpy()
+            with torch.no_grad():
+                pred = model.predict(input)
             end_time = time.time()
+            preds.append(pred.cpu().detach().numpy())
             predict_times.append((end_time - start_time) * 1000)
 
         avg_predict_time = round(np.mean(predict_times[2:]), 3)
-        print(f'    Predict Avg (ms): {avg_predict_time}\n')
+        print(f'    Avg Predict Time for shape {input_dim}: {avg_predict_time}ms\n')
 
 
 if __name__ == "__main__":
