ec3b5d88939837d8c28373302ca3358d3f3ed24d
RyanVNgo <vanryan711@gmail.com>
2025-04-22 02:25:56 -0700

    Options file and starting training implementation
    Set up yaml based option setting to control runtime parameters and starting implementation on training routine.

--- SISR/models/__init__.py
+++ SISR/models/__init__.py
@@ -5,6 +5,8 @@ from .archs.Baseline import Baseline
 from .archs.NAFNet import NAFNet
 
 __all__ = [
+    'create_sisr_model'
+
     # sisr_model.py
     'SISRModel',
     'sisr_network_types'
@@ -14,4 +16,61 @@ __all__ = [
     'Baseline'
     'NAFNet'
 ]
+
+
+def create_sisr_model(options):
+    network_options = options.get('network_arch', None)
+
+    if network_options is None:
+        return default_sisr_net()
+
+    net_type = network_options.get('type', 'PlainNet')
+    c_in =  network_options.get('c_in', 3)
+    width = network_options.get('width', 16)
+    enc_blk_nums = network_options.get('enc_blk_nums', [1, 1, 1])
+    dec_blk_nums = network_options.get('dec_blk_nums', [1, 1, 1])
+    mid_blk_num = network_options.get('mid_blk_num', 1)
+
+    device = options.get('device', 'cpu')
+
+    net = None
+    match net_type:
+        case 'PlainNet':
+            net = PlainNet(
+                c_in=c_in,
+                width=width,
+                mid_blk_num=mid_blk_num,
+                enc_blk_nums=enc_blk_nums,
+                dec_blk_nums=dec_blk_nums
+            )
+        case 'Baseline':
+            net = Baseline(
+                c_in=c_in,
+                width=width,
+                mid_blk_num=mid_blk_num,
+                enc_blk_nums=enc_blk_nums,
+                dec_blk_nums=dec_blk_nums
+            )
+        case 'NAFNet':
+            net = NAFNet(
+                c_in=c_in,
+                width=width,
+                mid_blk_num=mid_blk_num,
+                enc_blk_nums=enc_blk_nums,
+                dec_blk_nums=dec_blk_nums
+            )
+
+    return SISRModel(net, device)
+
+
+def default_sisr_net():
+    model = PlainNet (
+        c_in=3,
+        width=16,
+        mid_blk_num=1,
+        enc_blk_nums=[1, 1, 1],
+        dec_blk_nums=[1, 1, 1]
+    )
+    return model
+
 
--- SISR/train.py
+++ SISR/train.py
@@ -9,7 +9,7 @@ import numpy as np
 import torch
 import torch.nn as nn
 
-from utils import base_utils
+import utils
 import models
 from data import custom_dataset
 
@@ -23,68 +23,66 @@ def main():
         return
 
     args = parser.parse_args(sys.argv[1:])
+    options = utils.parse_options(args.opt)
 
-    # Placeholder for loading dataset
-    data_path = args.data_path
-    if not os.path.exists(data_path):
-        raise FileNotFoundError(f'Invalid dataset path: {data_path}\n')
-
-    dataset = custom_dataset.SISRDataset(data_path, crop=256)
-    if len(dataset) == 0:
-        raise FileNotFoundError(f'No valid images found in dataset path: {data_path}')
-
-    print(f'SISR Dataset loaded successfully from directory: {data_path}')
-    print(f'    Dataset length: {len(dataset)}')
-    print(f'    Dataset input shape: {dataset[0]['input'].shape}')
-    print(f'    Dataset target shape: {dataset[0]['target'].shape}\n')
-
-    # Placeholder for validating models
     device = 'cpu'
     if torch.cuda.is_available():
         device = torch.device('cuda')
 
-    c_in = 3
-    width = 16
-    enc_blk_nums = [1, 2, 4]
-    mid_blk_num = 1
-    dec_blk_nums = [4, 2, 1]
+    model = models.create_sisr_model(options.get('model'))
 
-    networks = [
-        models.PlainNet(c_in, width, mid_blk_num, enc_blk_nums, dec_blk_nums),
-        models.Baseline(c_in, width, mid_blk_num, enc_blk_nums, dec_blk_nums),
-        models.NAFNet(c_in, width, mid_blk_num, enc_blk_nums, dec_blk_nums)
-    ]
+    training_options = options.get('training', None)
+    optimizer = setup_optimizer(model.get_parameters(), training_options.get('optimizer'))
+    scheduler = setup_scheduler(optimizer, training_options.get('scheduler'))
+    iterations = training_options.get('iterations', 1)
 
-    sisr_models = []
-    for net in networks:
-        sisr_models.append(models.SISRModel(net, device))
+    return
 
-    for idx, model in enumerate(sisr_models):
-        print(f'Model {idx}:')
-        print(f'    Network type: {model.network_type()}')
-        print(f'    Channel depth: {model.input_channel_depth()}')
-        print(f'    Current Device: {model.curr_device()}')
 
-        runs = 20
-        print(f'    Integrity test:')
-        print(f'        Performing {runs} predicitions...')
+def setup_scheduler(optimizer, options):
+    scheduler_type = options.get('type', 'AdamW')
+    match scheduler_type:
+        case 'CosineAnnealingLR':
+            return torch.optim.lr_scheduler.CosineAnnealingLR(
+                optimizer=optimizer,
+                T_max=options.get('t_max'),
+                eta_min=options.get('eta_min', 0.0)
+            )
 
-        model.set_eval()
-        predict_times = []
-        for _ in range(runs):
-            input = dataset[random.randint(0, len(dataset) - 1)]['input']
-            input = input.unsqueeze(0).to(model.curr_device())
-            start_time = time.time()
-            with torch.no_grad():
-                pred = model.predict(input)
-            end_time = time.time()
-            predict_times.append((end_time - start_time) * 1000)
 
-        avg_predict_time = round(np.mean(predict_times[2:]), 3)
-        input_shape = dataset[0]['input'].numpy().shape
-        output_shape = pred.cpu().numpy().shape
-        print(f'    Avg Predict Time for shape {input_shape}: {avg_predict_time}ms')
-        print(f'        Output Shape: {output_shape}\n')
+def setup_optimizer(params, options):
+    optim_type= options.get('type', 'AdamW')
+    lr = options.get('lr', 1e-3)
+    match optim_type:
+        case 'Adam':
+            return torch.optim.Adam(
+                params=params,
+                lr=lr,
+                betas=options.get('betas', [0.9, 0.999]),
+                weight_decay=options.get('weight_decay', 0.0)
+            )
+        case 'AdamW':
+            return torch.optim.AdamW(
+                params=params,
+                lr=lr,
+                betas=options.get('betas', [0.9, 0.999]),
+                weight_decay=options.get('weight_decay', 0.01)
+            )
+        case 'RMSprop':
+            return torch.optim.RMSprop(
+                params=params,
+                lr=lr,
+                weight_decay=options.get('weight_decay', 0.0),
+                momentum=options.get('momentum', 0.0)
+            )
+        case 'SGD':
+            return torch.optim.SGD(
+                params=params,
+                lr=lr,
+                momentum=options.get('momentum', 0.0),
+                dampening=options.get('dampening', 0.0),
+                weight_decay=options.get('weight_decay', 0.0)
+            )
 
 
 def get_argparser():
@@ -93,7 +91,7 @@ def get_argparser():
         description='Training script for project models',
     )
 
-    parser.add_argument('-d', '--dataset', dest='data_path', help='Directory of training images')
+    parser.add_argument('-opt', type=str, required=True, help='Path to yaml file')
     return parser
 
 
