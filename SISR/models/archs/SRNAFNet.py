389e02e4e0e231a23ef1c14b5cee81866f3b057d
RyanVNgo <vanryan711@gmail.com>
2025-04-30 07:44:01 -0700

    Deep-SRNAFNet
    Implemented tentative Deep-SRNAFNet architecture and provided a baseline trained model with logs

--- SISR/models/archs/SRNAFNet.py
+++ SISR/models/archs/SRNAFNet.py
@@ -2,6 +2,7 @@
 import torch
 import torch.nn as nn
 
+'''
 class SRNAFNet(nn.Module):
     def __init__(self, c_in=3, width=16, mid_blk_num=1, intro_k=3, ending_k=3, block_opts=None):
         super().__init__()
@@ -65,10 +66,116 @@ class SRNAFNet(nn.Module):
         mod_pad_w = (self.padder_size - w % self.padder_size) % self.padder_size
         img = nn.functional.pad(img, (0, mod_pad_w, 0, mod_pad_h))
         return img
+'''
+
+class SRNAFNet(nn.Module):
+    def __init__(self, c_in=3, width=16, mid_blk_num=1, intro_k=3, ending_k=3, block_opts=None):
+        super().__init__()
+        dw_expand = 2
+        ffn_expand = 2
+        block_k = 1
+
+        if block_opts is not None:
+            dw_expand = block_opts.get('dw_expand', dw_expand)
+            ffn_expand = block_opts.get('ffn_expand', ffn_expand)
+            block_k = block_opts.get('kernel_size', block_k)
+
+        curr_channels = width
+
+        # ---- Shallow Feature Extraction Preparation ----
+        self.intro = nn.Conv2d(
+            in_channels = c_in,
+            out_channels = curr_channels,
+            kernel_size = intro_k,
+            padding = (intro_k - 1) // 2
+        )
+
+        # ---- Shallow Feature Extractors ----
+        # Small Scale Extraction
+        self.sfe_small = NAFNetBlock(curr_channels, dw_expand, ffn_expand, 3)
+
+        # Mid Scale Extraction
+        self.sfe_mid = NAFNetBlock(curr_channels, dw_expand, ffn_expand, 5)
+
+        # Wide Scale Extraction
+        self.sfe_wide = NAFNetBlock(curr_channels, dw_expand, ffn_expand, 7)
+
+        # ---- Deep Feature Extraction ----
+        curr_channels = curr_channels * 3
+        deep_fe_count = 3
+        self.deep_fe_blocks = nn.ModuleList()
+        for _ in range(deep_fe_count):
+            self.deep_fe_blocks.append(
+                NAFNetBlock(curr_channels, dw_expand, ffn_expand, 3)
+            )
+
+        # Deep Feature Upscaling 
+        self.deep_fe_post_upscale = nn.Sequential(
+            nn.PixelShuffle(2)
+        )
+        curr_channels = curr_channels // 4
+
+        # ---- Upscaled Feature Extraction ----
+        ufe_count = 3
+        self.ufe_blocks = nn.ModuleList()
+        for _ in range(ufe_count):
+            self.ufe_blocks.append(
+                NAFNetBlock(curr_channels, dw_expand, ffn_expand, 3)
+            )
+
+        # ---- Final Reconstruction ----
+        self.final_reconstruction = nn.Sequential(
+            nn.Conv2d(
+                curr_channels, 
+                3, 
+                kernel_size=ending_k,
+                padding=(ending_k - 1) // 2,
+            )
+        )
+
+        # Additional Stuff
+        self.c_in = c_in
+        self.padder_size = 1
+
+    def forward(self, input):
+        B, C, H, W = input.shape
+        input = self.conform_image_size(input)
+        input = self.intro(input)
+
+        # SFE
+        small_component = self.sfe_small(input)
+        mid_componenet = self.sfe_mid(input)
+        wide_component = self.sfe_wide(input)
+        concat = torch.cat((small_component, mid_componenet, wide_component), dim=1)
+
+        # DFE
+        input = concat
+        for dfe_block in self.deep_fe_blocks:
+            input = dfe_block(input)
+        input = input + concat
+
+        # DFU
+        input = self.deep_fe_post_upscale(input)
+
+        # UFE
+        for ufe_block in self.ufe_blocks:
+            input = ufe_block(input)
+
+        # Final Reconstruction
+        input = self.final_reconstruction(input)
+
+        return input
+
+    def conform_image_size(self, img):
+        _, _, h, w = img.size()
+        mod_pad_h = (self.padder_size - h % self.padder_size) % self.padder_size
+        mod_pad_w = (self.padder_size - w % self.padder_size) % self.padder_size
+        img = nn.functional.pad(img, (0, mod_pad_w, 0, mod_pad_h))
+        return img
 
 
 class NAFNetBlock(nn.Module):
-    def __init__(self, c_in, dw_expand=2, ffn_expand=2, kernel_size=1, dropout=0.0):
+    def __init__(self, c_in, dw_expand=2, ffn_expand=2, kernel_size=1):
         super().__init__()
 
         # First stage of block
@@ -106,9 +213,6 @@ class NAFNetBlock(nn.Module):
             ),
         )
 
-        # First dropout
-        self.dropout_1 = nn.Dropout(dropout) if dropout > 0.0 else nn.Identity()
-
         # Second stage of block
         self.norm_2 = LayerNorm2d(c_in)
         
@@ -126,13 +230,6 @@ class NAFNetBlock(nn.Module):
             padding = (kernel_size - 1) // 2
         )
 
-        # First dropout
-        self.dropout_2 = nn.Dropout(dropout) if dropout > 0.0 else nn.Identity()
-
-        # Not really sure wtf these are, they were in the open archs
-        self.beta = nn.Parameter(torch.zeros((1, c_in, 1, 1)), requires_grad=True)
-        self.gamma = nn.Parameter(torch.zeros((1, c_in, 1, 1)), requires_grad=True)
-
     def forward(self, input):
         x = self.norm_1(input)
         x = self.conv_1(x)
@@ -141,30 +238,51 @@ class NAFNetBlock(nn.Module):
         x = x * self.sca(x)
         x = self.conv_3(x)
 
-        x = self.dropout_1(x)
-
-        y = input + x * self.beta
+        y = input + x
 
         x = self.norm_2(y)
         x = self.conv_4(x)
         x = self.simple_gate(x)
         x = self.conv_5(x)
 
-        x = self.dropout_2(x)
+        return y + x
 
-        return y + x * self.gamma
+
+class LayerNormFunction(torch.autograd.Function):
+    @staticmethod
+    def forward(ctx, x, weight, bias, eps):
+        ctx.eps = eps
+        N, C, H, W = x.size()
+        mu = x.mean(1, keepdim=True)
+        var = (x - mu).pow(2).mean(1, keepdim=True)
+        y = (x - mu) / (var + eps).sqrt()
+        ctx.save_for_backward(y, var, weight)
+        y = weight.view(1, C, 1, 1) * y + bias.view(1, C, 1, 1)
+        return y
+
+    @staticmethod
+    def backward(ctx, grad_output):
+        eps = ctx.eps
+
+        N, C, H, W = grad_output.size()
+        y, var, weight = ctx.saved_variables
+        g = grad_output * weight.view(1, C, 1, 1)
+        mean_g = g.mean(dim=1, keepdim=True)
+
+        mean_gy = (g * y).mean(dim=1, keepdim=True)
+        gx = 1. / torch.sqrt(var + eps) * (g - y * mean_gy - mean_g)
+        return gx, (grad_output * y).sum(dim=3).sum(dim=2).sum(dim=0), grad_output.sum(dim=3).sum(dim=2).sum(dim=0), None
 
 
 class LayerNorm2d(nn.Module):
     def __init__(self, channels, eps=1e-6):
         super().__init__()
-        self.layer_norm = nn.LayerNorm(channels, eps=eps)
+        self.register_parameter('weight', nn.Parameter(torch.ones(channels)))
+        self.register_parameter('bias', nn.Parameter(torch.zeros(channels)))
+        self.eps = eps
 
     def forward(self, x):
-        x = x.permute(0, 2, 3, 1)
-        x = self.layer_norm(x)
-        x = x.permute(0, 3, 1, 2)
-        return x
+        return LayerNormFunction.apply(x, self.weight, self.bias, self.eps)
 
 
 class SimpleGate(nn.Module):
